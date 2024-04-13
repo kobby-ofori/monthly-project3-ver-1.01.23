6fa06ed67a76d8ba46bab877dba17f4cefdac84d
Elijah <122675800+kobby-ofori@users.noreply.github.com>
2024-04-13 10:29:31 +0000

    Create email.js

---
+++ email.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('#myForm').on('submit', function(event) {
+    event.preventDefault(); // prevent reload
+    
+    var formData = new FormData(this);
+    formData.append('service_id', 'YOUR_SERVICE_ID');
+    formData.append('template_id', 'YOUR_TEMPLATE_ID');
+    formData.append('user_id', 'YOUR_PUBLIC_KEY');
+ 
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+        type: 'POST',
+        data: formData,
+        contentType: false, // auto-detection
+        processData: false // no need to parse formData to string
+    }).done(function() {
+        alert('Your mail is sent!');
+    }).fail(function(error) {
+        alert('Oops... ' + JSON.stringify(error));
+    });
+  });
+})
